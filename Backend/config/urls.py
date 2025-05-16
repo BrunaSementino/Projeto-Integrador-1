d0379a7ab1eb249ccff35ee035a91cd19af23e4d
Bruna Sementino <brunasementino@gmail.com>
2025-05-16 17:59:52 -0300

    Delete Backend directory

--- Backend/config/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('estoque.urls')),  # <-- inclui as rotas da app estoque
-    path('dashboard/', views.dashboard, name='dashboard')
-
-]
